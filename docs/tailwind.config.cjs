b3ab7a6e90d810d3dc878b7721d26eb9425f2085
Ashriel <awaghmare@horizon.csueastbay.edu>
2023-08-01 20:28:09 +0530

    Added Hamburger Menu for SideBar
    Responsive on mobile devices. First draft

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -4,7 +4,6 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
     fontFamily: {
 		sans: ["Overpass", "sans-serif"],
 		body: ["Overpass", "sans-serif"],
